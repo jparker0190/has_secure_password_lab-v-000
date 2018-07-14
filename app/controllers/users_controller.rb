a3ac26d1a93164e9111b29b91b0cb50c9dffcb70
Joseph Parker <jparker010190@gmail.com>
2018-07-14 01:08:38 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
   def create
   user = User.new(user_params).save
   end
+  private
+
+ def user_params
+   params.require(:user).permit(:username, :password, :password_confirmation)
+ end
 end
